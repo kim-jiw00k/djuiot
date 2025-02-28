3939681131803e439d19e22713f21cc80c272b4d
kim-jiwook <100753019+kim-jiw00k@users.noreply.github.com>
2025-02-28 14:19:30 +0900

    projectproto.c

--- DataStructure/Project/project.c
+++ DataStructure/Project/project.c
@@ -14,13 +14,14 @@ car * head = NULL;	//링크드 리스트의 헤드 포인터
 void screen();	//초기 화면
 void add_screen();		//추가 화면
 void print_screen();	//조회 화면
-//void find_screen();		//찾기 화면
+void clear();			//종료 화면
 int add(car ** carnum, char * str);	//추가 하는 함수
 
 
 int main()
 {
 	screen();
+	clear(&head);
 
 	return 0;
 }
@@ -28,7 +29,7 @@ void screen()
 {
 	int choice = 0;
 	printf("\t\t***** 주차 관리 시스템 *****\n");
-	printf("번호를 입력하시면 이동합니다.\n1. 차량 추가  2. 차량 조회  3. 차량 검색\n");
+	printf("번호를 입력하시면 이동합니다.\n1. 차량 추가  2. 차량 조회  3. 종료\n");
 	printf("번호 입력 : ");
 	scanf("%d", &choice);
 	if(choice == 1)
@@ -47,10 +48,10 @@ void screen()
 	}
 	else if(choice == 3)
 	{
-		printf("차량 검색으로 이동합니다.\n");
+		printf("종료 합니다.\n");
 		sleep(1);
 		system("clear");
-		//find_screen();
+		return;
 	}
 	else
 	{
@@ -84,17 +85,16 @@ int add(car ** carnum, char * str)
 void add_screen()		//추가 화면 함수
 {
 	char carn[100];
-	car * head = NULL;
 	printf("***** 차량 번호 추가 *****\n");
 	printf("추가할 차량 번호를 입력하시오. : ");
-	scanf("%s", &carn);
+	scanf("%s", carn);
 	add(&head, carn);		//head에다가 carn을 추가 한다.
 	printf("추가 완료! \n");
 	sleep(1);
 	system("clear");
 	screen();
 }
-/*
+
 void print_screen()		//조회 화면 함수
 {
 	int ex = 0;
@@ -109,6 +109,18 @@ void print_screen()		//조회 화면 함수
 	if(ex != 0)
 	{
 		system("clear");
-		index_win();
+		screen();
 	}
-}*/
+}
+
+void clear()
+{
+	car * current = head;
+	while(current != NULL)
+	{
+		car * link = current -> link;
+		free(current);
+		current = link;
+	}
+	head = NULL;
+}
